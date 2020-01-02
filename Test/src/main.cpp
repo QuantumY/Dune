bd8a3d1c98b59eb1a2589e383eb89b20811a9246
Jeff Doss <jdosstech@gmail.com>
2020-01-02 11:32:12 -0800

    Refactored DUNE includes out of main.cpp in app

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,11 +1,3 @@
-/*
-#define DUNE_PLATFORM_WINDOWS
-#define DUNE_PLATFORM_X64
-#define DUNE_COMPILE_STATIC
-#define DUNE_DEBUG
-#define DUNE_EXIT_ON_FATAL_ERROR
-*/
-
 #include "Dune.h"
 
 int main(int argc, char* args[])
